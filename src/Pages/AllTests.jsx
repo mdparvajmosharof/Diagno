548f98a9bb67e75803e314bcfabf8f339335d0c7
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2024-10-05 22:46:27 +0600

    added search function for priceRange, name at all tests section

--- src/Pages/AllTests.jsx
+++ src/Pages/AllTests.jsx
@@ -13,6 +13,8 @@ import { Pagination } from 'swiper/modules';
 const AllTests = () => {
   const axiosPublic = useAxiosPublic();
   const [searchDate, setSearchDate] = useState('');
+  const [priceRange, setPriceRange] = useState([0, 2000]);
+  const [name, setName] = useState('');
   const [tests, setTests] = useState([]);
   const [loading, setLoading] = useState(true)
 
@@ -26,11 +28,13 @@ const AllTests = () => {
   }, [axiosPublic]);
 
   const handleSearch = () => {
-    axiosPublic.get(`/testsDate?date=${searchDate}`)
+    axiosPublic.get(`/testsDate?date=${searchDate}&minPrice=${priceRange[0]}&maxPrice=${priceRange[1]}&name=${name}`)
       .then(res => {
         setTests(res.data);
       });
   };
+
+  // console.log(priceRange, priceRange[0], priceRange[1])
 
   const pagination = {
     clickable: true,
@@ -40,13 +44,13 @@ const AllTests = () => {
   }
 
   const groupedTests = [];
-  for (let i = 0; i <= tests.length ; i += 9) {
+  for (let i = 0; i <= tests.length; i += 9) {
     groupedTests.push(tests.slice(i, i + 9));
   }
 
   // console.log(groupedTests)
 
-  
+
 
 
   if (loading) {
@@ -55,13 +59,45 @@ const AllTests = () => {
 
   return (
     <div>
-      <div className="flex justify-center mt-6">
-        <input
-          type="date"
-          value={searchDate}
-          onChange={(e) => setSearchDate(e.target.value)}
-          className="input input-bordered"
-        />
+      <div className="flex justify-center mt-6 gap-2">
+
+        <label className='input input-bordered flex items-center gap-2' >Date :
+          <input
+            id='date'
+            type="date"
+            value={searchDate}
+            onChange={(e) => setSearchDate(e.target.value)}
+            className="grow"
+          />
+        </label>
+
+        <label className='input input-bordered flex items-center gap-2' >Min Price :
+          <input
+            type="number"
+            value={priceRange[0]}
+            onChange={(e) => setPriceRange([e.target.value, priceRange[1]])}
+            className="grow"
+            />
+        </label>
+
+        <label className='input input-bordered flex items-center gap-2' >Max Price :
+          <input
+            type="number"
+            value={priceRange[1]}
+            onChange={(e) => setPriceRange([priceRange[0],e.target.value])}
+            className="grow"
+          />
+        </label>
+
+        <label className='input input-bordered flex items-center gap-2' >Name :
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="grow"
+          />
+        </label>
+
         <button onClick={handleSearch} className="btn btn-primary ml-2">Search</button>
       </div>
       <div className='flex justify-center'>
